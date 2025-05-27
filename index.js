a73529a02c6d8afd6404bb5630d4649f651b718b
yoppy3030 <hinata.3131abc@gmail.com>
2025-05-27 15:36:15 +0900

    最新

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const express = require('express'); // 例としてExpressを使用
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// publicフォルダを静的ファイルの提供元として設定
+app.use(express.static('public'));
+
 // ルートエンドポイントの設定
 app.get('/', (req, res) => {
-    res.send('Hello, World!'); // 簡単なレスポンス
+    res.sendFile(__dirname + '/public/index.html'); // index.htmlを返す
 });
 
 // サーバーの起動
