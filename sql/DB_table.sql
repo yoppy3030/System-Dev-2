40a923cd106215bacc2c82d1dd6e905a58283d76
yoppy3030 <hinata.3131abc@gmail.com>
2025-05-30 14:50:38 +0900

    最新　5/30/14:50

---
+++ sql/DB_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Accounts (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(50) NOT NULL COMMENT 'ユーザーの名前',
+    Password VARCHAR(255) NOT NULL COMMENT 'ユーザーのパスワード',
+    Email VARCHAR(100) NOT NULL UNIQUE COMMENT 'ユーザーのメールアドレス',
+    Country VARCHAR(50) NOT NULL COMMENT 'ユーザーの国',
+    Current_location VARCHAR(100) NOT NULL COMMENT 'ユーザーの現在の所在地',
+    UserType ENUM('Tourist', 'International Student', 'Professional' , 'other') NOT NULL COMMENT 'ユーザーのタイプ',
+    RegistrationDate DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '登録日時',
+    LastLogin DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '最終ログイン日時',
+    Prefecture VARCHAR(50) COMMENT 'ユーザーの都道府県'
+);
