619b238b4d766dee4b1a93ff7787ea174c5adcbf
yoppy3030 <hinata.3131abc@gmail.com>
2025-06-06 15:10:56 +0900

    logo を統一

--- src/test/examination.php
+++ src/test/examination.php
@@ -8,7 +8,7 @@
 </head>
 <body>
   <header class="site-header">
-    <div class="logo">STUDY in JAPAN</div>
+    <div class="logo">JAPAN Life Manual</div>
     <nav class="main-nav">
       <a href="studenthome.php">StudentHome</a>
       <a href="lifeinjapan.php">Life in Japan</a>
