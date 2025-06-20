45b5d001a8f9837562864a506f6c95aa62286d6e
2240707S\2240707 <aungmyominpro1995@gmail.com>
2025-06-20 14:49:53 +0900

    few add and changes

--- src/test/studenthome.php
+++ src/test/studenthome.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>JAPAN Life Manual - Custom Clone</title>
+  <title>JAPAN Life Manual</title>
   <link rel="stylesheet" href="./css/studenthome.css">
 </head>
 <body>
